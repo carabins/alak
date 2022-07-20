56e4753c3e7667803808883b2bf96fb7d011b8e3
Gleb Panteleev <dev@gleb.pw>
2022-07-20 19:59:36 +0300

    + согласованность ключей мультихранилища

---
+++ packages/molecule/src/PartOfMolecule.ts
@@ -0,0 +1,3 @@
+/*
+ * Copyright (c) 2022. Only the truth - liberates.
+ */
---
+++ packages/molecule/types/events.ts
@@ -0,0 +1,6 @@
+/*
+ * Copyright (c) 2022. Only the truth - liberates.
+ */
+
+type MoleculeEvents = '' | 'CLICK' | string
+interface MoleculeAtoms {}
