b83e0ab21d3d6741d62f887cdf46e1d5aa7a3846
Gleb Panteleev <dev@gleb.pw>
2022-07-22 10:57:20 +0300

    +

--- packages/molecule/src/index.ts
+++ packages/molecule/src/index.ts
@@ -18,10 +18,11 @@ export function getMolecule(id: string = 'molecule'): ActiveMolecule {
   return am
 }
 
-export class PartOfMolecule {
+export abstract class PartOfMolecule {
   _: {
-    dispatchEvent(name: MoleculeEvents, data?: any)
     atoms: MoleculeAtoms
+    dispatchEvent(name: MoleculeEvents, data?): void
+    set(atom: string, nuclon: string, data: any): void
+    get(atom: string, nuclon): void
   }
-  q: any
 }
--- packages/molecule/src/moleculeExtension.ts
+++ packages/molecule/src/moleculeExtension.ts
@@ -3,10 +3,25 @@
  */
 
 export default function (q: QuantumAtom) {
+  const getA = (name) => {
+    const a = q.molecule.atoms[name]
+    if (!a) {
+      console.error(`обращение из атома [${q.name}] к отсуствующему атому [${name}]`)
+    }
+    return a
+  }
   return {
     _: {
       dispatchEvent: q.molecule.eventBus,
       atoms: q.molecule.atoms,
+      set(atom: string, nuclon: string, data: any) {
+        const a = getA(atom)
+        a && a.core[nuclon](data)
+      },
+      get(atom: string, nuclon) {
+        const a = getA(atom)
+        return a ? a.core[nuclon].value : new Error('atom not found')
+      },
     },
   }
 }
