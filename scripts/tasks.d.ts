6bc95b57f1e346ccb3c373b26eb508702c2638ad
ozz <dev@gleb.pw>
2026-01-12 18:25:03 +0300

    docs: add build instructions and update tasks definition

--- scripts/tasks.d.ts
+++ scripts/tasks.d.ts
@@ -1,4 +1,4 @@
 // generated from A:\source\alak-v5\scripts\tasks
-declare type BuildBaseTaskNames = "browser" | "clear" | "compile" | "coverage-text" | "coverage" | "test" | "build" | "package"
+declare type BuildBaseTaskNames = "browser" | "clear" | "compile" | "build" | "package" | "coverage-text" | "coverage" | "test"
 declare type BuildAggregateTaskNames = "full-clear" | "tsc" | "lcov-to-html" | "lcov"
 
